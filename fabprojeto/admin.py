c7af58698323a6091af66642ff4dfc45d1408a31
Pedro Oliveira <oliveiradrtpedro@gmail.com>
2022-09-16 13:31:53 -0300

    models

--- fabprojeto/admin.py
+++ fabprojeto/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Categorias, Reclamacoes
+
 
 # Register your models here.
+
+admin.site.register(Categorias)
+admin.site.register(Reclamacoes)
--- fabprojeto/models.py
+++ fabprojeto/models.py
@@ -1,3 +1,19 @@
+from ctypes import resize
+from gettext import NullTranslations
+from pydoc import describe
 from django.db import models
 
 # Create your models here.
+
+
+class Categorias(models.Model):
+    nome = models.CharField( max_length=50)
+    horario = models.DateTimeField(auto_now_add=True)
+
+
+class Reclamacoes(models.Model):
+    data = models.DateField()
+    assunto = models.CharField(max_length=100)
+    valor = models.DecimalField(max_digits=7, decimal_places=2)
+    categoria  = models.ForeignKey(Categorias, on_delete=models.CASCADE)
+    descricao = models.TextField(null=True, blank=True)
